fb46fa2766de46c324d5974df5fa480c9fb84e9f
sashaknw <kaniewa.aleksandra@gmail.com>
2025-01-20 14:55:19 +0000

    day 1 quiz

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,37 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+   
 
-    // 2. getQuestion()
+     constructor (questions, timeLimit, timeRemaining) {
+        this.questions = questions;
+        this.timeLimit = timeLimit;
+        this. timeRemaining = timeRemaining;
+        this.correctAnswers = 0;
+        this.currentQuestionIndex = 0;
+     }
+
+     getQuestion() {
+        return this.questions[this.currentQuestionIndex];
+     }
     
-    // 3. moveToNextQuestion()
+    moveToNextQuestion() {
+        this.currentQuestionIndex++;
+    }
 
-    // 4. shuffleQuestions()
+     shuffleQuestions(answer) {
+        this.questions.sort(() => Math.random() - 0.5);
+     }
 
-    // 5. checkAnswer(answer)
+     checkAnswer(answer) {
+        this.correctAnswers++;
+     }
 
-    // 6. hasEnded()
+    hasEnded(){
+        if (this.currentQuestionIndex < this.questions.length) {
+            return false;
+        } else {
+            return true;
+        }
+        
+    }
+    
 }
