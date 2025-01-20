7efb104b2fb6ab322c83eff581183530e24c213b
Airam Plasencia <airaplas@gmail.com>
2025-01-20 17:35:59 +0000

    filter and reduced update

--- src/quiz.js
+++ src/quiz.js
@@ -1,37 +1,53 @@
 class Quiz {
-   
 
-     constructor (questions, timeLimit, timeRemaining) {
+
+    constructor(questions, timeLimit, timeRemaining) {
         this.questions = questions;
         this.timeLimit = timeLimit;
-        this. timeRemaining = timeRemaining;
+        this.timeRemaining = timeRemaining;
         this.correctAnswers = 0;
         this.currentQuestionIndex = 0;
-     }
+    }
 
-     getQuestion() {
+    getQuestion() {
         return this.questions[this.currentQuestionIndex];
-     }
-    
+    }
+
     moveToNextQuestion() {
         this.currentQuestionIndex++;
     }
 
-     shuffleQuestions(answer) {
+    shuffleQuestions(answer) {
         this.questions.sort(() => Math.random() - 0.5);
-     }
+    }
 
-     checkAnswer(answer) {
+    checkAnswer(answer) {
         this.correctAnswers++;
-     }
+    }
 
-    hasEnded(){
+    hasEnded() {
         if (this.currentQuestionIndex < this.questions.length) {
             return false;
         } else {
             return true;
         }
-        
+
     }
-    
+    filterQuestionsByDifficulty(difficulty) {
+        if (difficulty > 0 && difficulty < 4) {
+
+            const filteredQuestions = this.questions.filter(question => question.difficulty === difficulty);
+            this.questions = filteredQuestions
+
+        }
+
+
+    }
+averageDifficulty() {
+const sumDifficulty = this.questions.reduce(function(sum, question) {
+    return sum + question.difficulty;
+
+}, 0);
+    return sumDifficulty / this.questions.length;
+}
 }
